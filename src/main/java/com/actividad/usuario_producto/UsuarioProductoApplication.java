c9fdae513961b48987a7281ebe9f7b18a0d7d7d8
Andres <arperdomo26@gmail.com>
2024-10-01 18:35:03 -0500

    is create the file usuario_producto

---
+++ src/main/java/com/actividad/usuario_producto/UsuarioProductoApplication.java
@@ -0,0 +1,13 @@
+package com.actividad.usuario_producto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuarioProductoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsuarioProductoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/actividad/usuario_producto/UsuarioProductoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.actividad.usuario_producto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuarioProductoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
